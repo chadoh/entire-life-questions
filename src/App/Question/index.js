67a1dbd9073f65344b96bdc75533b13cb218d27f
Chad Ostrowski <hi@chadoh.com>
2018-01-01 14:05:03 -0500

    Style Question component; add Ace Editor dep

--- src/App/Question/index.js
+++ src/App/Question/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Editor from './Editor';
+import './Question.css'
+
+const change = attr => newValue => {
+  console.log('new ' + attr, newValue);
+}
 
 export default ({template, questions}) => <React.Fragment>
-  <h1>The Event Template</h1>
+  <div><Link to="/">Entire.Life Questions</Link></div>
+  <h1>{questions.ask}</h1>
+
+  <h2 className="Question-h2">Event Template</h2>
   <p>
-    When you answer the questions, the created event will look a bit like
-    this. Learn more about the templating system they use.
+    After someone answers this questions &amp; all follow-ups, an event will be
+    created that looks a bit like the following. Learn more about the
+    templating system.
   </p>
-  <textarea value={JSON.stringify(template)} />
-  <h1>The Question Set</h1>
+  <Editor
+    onChange={change('template')}
+    value={template}
+    height="130px"
+  />
+
+  <h2 className="Question-h2">Question Set</h2>
   <p>
     A question set forms a sort of flow chart. The answer to one question
-    determines which question you see next. Once the end of a flow is reached,
-    the save button is shown. Clicking the "save" button fires a "save" event
+    determines which question is shown next. Once the end of a flow is reached,
+    the save button is shown. Clicking the save button fires a "save" event
     with a filled-in event template.
   </p>
-  <textarea value={JSON.stringify(questions)} />
+  <Editor
+    onChange={change('questions')}
+    value={questions}
+    height="50vh"
+  />
 </React.Fragment>
