60e89f264469aca8c6bc82d2e86adad560220aa1
Chad Ostrowski <hi@chadoh.com>
2018-01-07 20:37:16 -0500

    Add Demo to Question page

--- src/App/Question/index.js
+++ src/App/Question/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { QuestionSet } from 'react-quizzical';
 
+import Columns from '../Columns';
 import Editor from './Editor';
 import './Question.css'
 
@@ -12,29 +14,41 @@ export default ({template, questions}) => <React.Fragment>
   <div><Link to="/">Entire.Life Questions</Link></div>
   <h1>{questions.ask}</h1>
 
-  <h2 className="Question-h2">Question Set</h2>
-  <p>
-    A question set forms a sort of flow chart. The answer to one question
-    determines which question is shown next. Once the end of a flow is reached,
-    the save button is shown. Clicking the save button fires a "save" event
-    with a filled-in event template (see below). <Link
-    to="/help/question-sets">Learn more about Question Sets</Link>.
-  </p>
-  <Editor
-    onChange={change('questions')}
-    value={questions}
-    height="50vh"
-  />
+  <Columns>
+    <div>
+      <h2 className="Question-h2">Question Set</h2>
+      <p>
+        A question set forms a sort of flow chart. The answer to one question
+        determines which question is shown next. Once the end of a flow is reached,
+        the save button is shown. Clicking the save button fires a "save" event
+        with a filled-in event template (see below). <Link
+        to="/help/question-sets">Learn more about Question Sets</Link>.
+      </p>
+      <Editor
+        onChange={change('questions')}
+        value={questions}
+        height="50vh"
+      />
 
-  <h2 className="Question-h2">Event Template</h2>
-  <p>
-    After someone answers all the questions in the Question Set, an event will
-    be created that looks a bit like the following. <Link
-    to="/help/templates">Learn more about the templating system</Link>.
-  </p>
-  <Editor
-    onChange={change('template')}
-    value={template}
-    height="130px"
-  />
+      <h2 className="Question-h2">Event Template</h2>
+      <p>
+        After someone answers all the questions in the Question Set, an event will
+        be created that looks a bit like the following. <Link
+        to="/help/templates">Learn more about the templating system</Link>.
+      </p>
+      <Editor
+        onChange={change('template')}
+        value={template}
+        height="150px"
+      />
+    </div>
+
+    <div>
+      <h2 className="Question-h2">Demo</h2>
+      <QuestionSet
+        questions={questions}
+        template={template}
+      />
+    </div>
+  </Columns>
 </React.Fragment>
